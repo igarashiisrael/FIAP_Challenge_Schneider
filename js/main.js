de6700c946fe8ddb48e591ce434deabe8232f240
Matheus Gouvêa <matheus-mcgcruz@hotmail.com>
2023-08-21 18:59:36 -0300

    funções

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var botao = document.getElementById('btn-invest');
+var totalinvest = document.getElementById('num-invest');
+var cont = 0;
+const telefoneInput = document.querySelector('.valida-telefone');
+telefoneInput.addEventListener('input', formatarTelefone);
+
+
+function validarEmail() {
+  var emailInput = document.querySelector('.input-email');
+  var email = emailInput.value;
+  var emailRegex = /\S+@\S+\.\S+/;
+
+  if (emailRegex.test(email)) {
+    alert("Seu e-mail foi cadastrado, muito obrigado!");
+  } else {
+    alert("Por favor, insira um e-mail válido.");
+  }
+}
+
+function validarEmailIndex() {
+  var emailInput = document.querySelector('.input-email-index');
+  var email = emailInput.value;
+  var emailRegex = /\S+@\S+\.\S+/;
+
+  if (emailRegex.test(email)) {
+    alert("Seu e-mail foi cadastrado, muito obrigado!");
+  } else {
+    alert("Por favor, insira um e-mail válido.");
+  }
+}
+
+function validarTelefone() {
+  const telefoneInput = document.querySelector('.valida-telefone');
+  const telefone = telefoneInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  const ddd = telefone.slice(0, 2);
+  const numero = telefone.slice(2);
+
+  const telefoneFormatado = `(${ddd}) ${numero}`;
+
+  if (!telefone) {
+    return;
+  }
+  if (telefone.length !== 11 || isNaN(telefone)) {
+    alert('Digite um número de telefone válido!');
+    return;
+  } else { alert('Telefone cadastrado com sucesso!') }
+}
+
+function formatarTelefone() {
+  const telefone = telefoneInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+  const ddd = telefone.slice(0, 2);
+  const primeiraParte = telefone.slice(2, 7);
+  const segundaParte = telefone.slice(7);
+
+  let telefoneFormatado = `(${ddd}) ${primeiraParte}`;
+
+  if (segundaParte) {
+    telefoneFormatado += `-${segundaParte}`;
+  }
+
+  telefoneInput.value = telefoneFormatado;
+}
